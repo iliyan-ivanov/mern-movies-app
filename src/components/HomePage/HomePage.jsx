8633f2f06a3e908628a7ae35cfc4188fe4fcaf3a
Iliyan Ivanov <iliyan.ivanov9428@gmail.com>
2024-08-24 23:08:32 +0300

    testing home page cards

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,11 +1,46 @@
 import "./HomePage.css";
 
 const HomePage = () => {
-    return (
-        <main>
-            Movies...
-        </main>
-    )
-}
+  return (
+    <main className="movies">
+      <div className="movie-card">
+        <img
+          src="https://i.ebayimg.com/images/g/ZKcAAOSwQXlcGzDu/s-l1200.jpg"
+          alt="Movie poster"
+        />
+      </div>
+      <div className="movie-card">
+        <img
+          src="https://i.ebayimg.com/images/g/ZKcAAOSwQXlcGzDu/s-l1200.jpg"
+          alt="Movie poster"
+        />
+      </div>
+      <div className="movie-card">
+        <img
+          src="https://i.ebayimg.com/images/g/ZKcAAOSwQXlcGzDu/s-l1200.jpg"
+          alt="Movie poster"
+        />
+      </div>
+      <div className="movie-card">
+        <img
+          src="https://i.ebayimg.com/images/g/ZKcAAOSwQXlcGzDu/s-l1200.jpg"
+          alt="Movie poster"
+        />
+      </div>
+      <div className="movie-card">
+        <img
+          src="https://i.ebayimg.com/images/g/ZKcAAOSwQXlcGzDu/s-l1200.jpg"
+          alt="Movie poster"
+        />
+      </div>
+      <div className="movie-card">
+        <img
+          src="https://i.ebayimg.com/images/g/ZKcAAOSwQXlcGzDu/s-l1200.jpg"
+          alt="Movie poster"
+        />
+      </div>
+    </main>
+  );
+};
 
 export default HomePage;
