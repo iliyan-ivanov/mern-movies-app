684a9813d643dabd66eefe53ce0d4db5b2bac7c2
Iliyan Ivanov <iliyan.ivanov9428@gmail.com>
2024-08-29 15:02:23 +0300

    Renaming component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Header from "./components/Header/Header";
 import HomePage from "./components/HomePage/HomePage";
-import Categories from "./components/Categories/Categories";
+import TypesPage from "./components/TypesPage/TypesPage";
 import CreatePage from "./components/CreatePage/CreatePage";
 import MovieDetailsPage from "./components/MovieDetailsPage/MovieDetailsPage";
 import "./App.css";
@@ -13,7 +13,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/types/:type" element={<Categories />} />
+        <Route path="/types/:type" element={<TypesPage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/movies/:id" element={<MovieDetailsPage />} />
       </Routes>
