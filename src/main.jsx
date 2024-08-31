d85dcf0a17f9252aeb906112ee7e9701f823feee
Iliyan Ivanov <iliyan.ivanov9428@gmail.com>
2024-08-31 19:20:46 +0300

    Application Deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename="/mern-movies-app/">
       <App />
     </BrowserRouter>
   </StrictMode>
