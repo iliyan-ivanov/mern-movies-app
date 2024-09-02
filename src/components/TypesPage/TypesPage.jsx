3d6381a02db94829b263f8a17cb72ff7c6cf82f8
Iliyan Ivanov <iliyan.ivanov9428@gmail.com>
2024-09-02 13:06:15 +0300

    Loading spinner added

--- src/components/TypesPage/TypesPage.jsx
+++ src/components/TypesPage/TypesPage.jsx
@@ -1,8 +1,9 @@
 import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ColorRing } from 'react-loader-spinner';
 import { getAllMovies } from "../../services/MovieServices";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import MovieCard from "../MovieCard/MovieCard";
-import { useParams } from "react-router-dom";
 
 const TypesPage = () => {
   const [ movies, setMovies ] = useState();
@@ -20,12 +21,26 @@ const TypesPage = () => {
         theme == "light" ? "movies-light movies" : "movies-dark movies"
       }
     >
-      {movies 
-       ? movies?.map((x) => (
-        <MovieCard img={x.imageUrl} key={x._id} id={x._id} />
-      ))
-       : <div>Loading movies ...</div>
-       }
+      {movies ? (
+        movies?.map((x) => (
+          <MovieCard img={x.imageUrl} key={x._id} id={x._id} />
+        ))
+      ) : (
+        <article className="loading-spinner">
+          <div style={{ color: theme === "light" ? "black" : "white" }}>
+            Loading movies ...
+          </div>
+          <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="color-ring-loading"
+            wrapperStyle={{}}
+            wrapperClass="color-ring-wrapper"
+            colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+          />
+        </article>
+      )}
     </main>
   );
 };
